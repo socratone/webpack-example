d04c15fd5dcdd713b7e034a7128d711160a18078
Kiwon Kim <kiwon@elicer.com>
2023-08-20 15:26:08 +0900

    fix: HtmlWebpackPlugin typo

--- html-plugin/webpack.config.js
+++ html-plugin/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,13 +13,13 @@ module.exports = {
   },
   plugins: [
     // dist에 추가적으로 html 파일을 만들어주는 플러그인
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/first.html'), // html을 생성할 때 재료로 삼을 .html 파일 경로
       chunks: ['first'], // entry의 'first'에 해당하는 .js 파일을 bundle로 사용
       inject: 'body', // 'body'를 넣으면 모든 javascript resource는 body의 제일 끝에 위치하게 된다.
       filename: 'first.html', // 'first.html'라는 이름으로 파일 생성
     }),
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/second.html'),
       chunks: ['second'],
       inject: 'body',
